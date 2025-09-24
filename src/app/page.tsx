04a7b5faa3d9cefdf3c9f2e755835e4ca91d3130
Dudex-dev <159432385+Dudex-dev@users.noreply.github.com>
2025-09-24 14:40:03 -0300

    Quase OK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+import Header from "./_components/header";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { SearchIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="mb-8 border-4 border-solid border-blue-500 p-6 pl-10">
-      <h1 className="bg-black text-red-500">Home page</h1>
-      <Button>Click me</Button>
+    <div>
+    <div>
+      <Header />
+      <p className="text-xl font-bold">Olá, Dudex</p>
+      <p className="Text-sm">Sabádo, 24 de Janeiro de 2025</p>
+    </div>
+
+    <div className="mt-4 flex flex-row items-center gap-2">
+        <Input placeholder="Faça sua busca..." />
+        <Button size={"icon"}>
+          <SearchIcon />
+        </Button>
+    </div>
     </div>
   );
 }
